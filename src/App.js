9d864db248f457a6b1ae00d821ec65f05c1a2718
Anelisy Nagano <anelisynagano@gmail.com>
2019-04-15 15:56:11 +0100

    start repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Lamp from "./Lamp";
+
+// import Quote from "./Quote";
+import Quotes from "./Quotes";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      working: true
+    };
+  }
+  handleClick = () => {
+    this.setState({ working: !this.state.working });
+  };
+
   render() {
+    const appLogo2 = this.state.working ? 'App-logo' : 'App-logo-2';
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img src={logo} className={appLogo2} alt="logo" />
+          <h1 className="App-title">Simpsons Quotes</h1>
+          <button onClick={this.handleClick}>Working</button>
         </header>
+
+        {/* <Quote
+          quote="I believe the children are the future... Unless we stop them now!"
+          character="Homer Simpson"
+          image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939"
+        />
+        <Quote
+          quote="Me fail English? That's unpossible"
+          character="Ralph Wiggum"
+          image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FRalphWiggum.png?1497567511523"
+        /> */}
+        <Lamp on />
+        <Lamp />
+        <Quotes />
       </div>
     );
   }
